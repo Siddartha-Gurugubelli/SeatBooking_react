592b8191fbe2f92f3df3dd9889ff993773851fbc
Saravani Nemani <saravaninemani@gmail.com>
2022-05-09 09:06:55 +0530

    Add files via upload

---
+++ src/components/Admin.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+ function Admin() {
+    return (
+        
+        <div>
+            <h1 align="center">Welcome Admin</h1>         
+        </div>        
+       
+        )    
+    }
+    export default Admin;
+
+
+
